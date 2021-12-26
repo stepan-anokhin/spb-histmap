54268dfe0f842827ccfc3cbf78a5fd07c1742e59
Stepan Anokhin <stepan.anokhin@gmail.com>
2021-12-26 23:58:41 +0700

    Install mui, mui-icons and Roboto font

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 import Application from "./components/Application";
+
 import hits from "./data";
 
 ReactDOM.render(<Application hits={hits} />, document.querySelector("#root"));
